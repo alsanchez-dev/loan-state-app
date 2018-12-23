2b164159adca3ac30eec9281fcf8b04a16d512ba
Albert Sanchez <al@Alberts-MacBook-Pro.local>
2018-12-23 16:36:00 -0600

    added clients array to Clients

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 
 class Clients extends Component {
     render() {
+        const clients = [{
+            id: '4242523523',
+            firstName: 'Kevin',
+            lastName: 'Johnson',
+            email: 'kevin@gmail.com',
+            phone: '555-555-5555',
+            balance: '30'
+        }]
         return (
             <div>
                 <h1>Clients</h1>
