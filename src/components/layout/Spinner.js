4a38ec4dd3dbff43346d5d33e80624904159222a
Albert Sanchez <al@Alberts-MacBook-Pro.local>
2018-12-23 21:31:16 -0600

    added AddClient component

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -4,7 +4,7 @@ import spinner from './spinner.gif';
 export default () => {
     return (
         <div>
-            <img src={spinner} alt="Loading..." style={{width: '100px', margin: 'auto', display: 'block'}} />
+            <img src={spinner} alt="Loading..." style={{width: '130px', margin: 'auto', marginTop:'10vh', display: 'block'}} />
         </div>
     );
 };
