07f3605514715595923e730f571f7df8503079a7
Albert Sanchez <al@Alberts-MacBook-Pro.local>
2018-12-24 07:33:45 -0600

    added component to add clients and is now working connected to firebase and the main dashboard

--- src/components/clients/AddClient.js
+++ src/components/clients/AddClient.js
@@ -18,6 +18,16 @@ class AddClient extends Component {
         e.preventDefault();
 
         const newClient = this.state;
+
+        const { firestore, history } = this.props;
+
+        // if no balance, make 0
+        if(newClient.balance === ''){
+            newClient.balance = 0;
+        }
+
+        firestore.add({ collection: 'clients' }, newClient)
+            .then(()=> history.push('/'))
     }
 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value });
@@ -37,7 +47,7 @@ class AddClient extends Component {
                     <div className="card">
                         <div className="card-header">Add Client</div>
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
                                     <label htmlFor="firstName">First Name</label>
                                     <input
@@ -99,6 +109,11 @@ class AddClient extends Component {
                                     />
                                 </div>
 
+                                <input
+                                    type="submit"
+                                    value="Submit"
+                                    className="btn btn-primary btn-block"
+                                />
                             </form>
                         </div>
                     </div>
@@ -106,97 +121,10 @@ class AddClient extends Component {
             );
         }
 
-
-//     render() {
-//         return (
-//
-//             <div>
-//                 <div className="row">
-//                     <div className="col-md-6">
-//                         <Link to="/" className="btn btn-link">
-//                             <i className="fas fa-arrow-circle-left" /> Back To Dashboard
-//                         </Link>
-//                     </div>
-//                 </div>
-//
-//                 <div className="card">
-//                     <div className="card-header">Add Client</div>
-//                     <div className="card-body">
-//                         <form onSubmit={this.onSubmit} >
-//                             <div className="form-group">
-//                                 <label htmlFor="firstName">First Name</label>
-//                                 <imput
-//                                     type="text"
-//                                     className="form-control"
-//                                     name="firstName"
-//                                     minLength="2"
-//                                     required
-//                                     onChange={this.onChange}
-//                                     value={this.state.firstName}
-//                                 />
-//                             </div>
-//
-//                             <div className="form-group">
-//                                 <label htmlFor="lastName">Last Name</label>
-//                                 <imput
-//                                     type="text"
-//                                     className="form-control"
-//                                     name="lastName"
-//                                     minLength="2"
-//                                     required
-//                                     onChange={this.onChange}
-//                                     value={this.state.lastName}
-//                                 />
-//                             </div>
-//
-//                             <div className="form-group">
-//                                 <label htmlFor="email">Email</label>
-//                                 <imput
-//                                     type="email"
-//                                     className="form-control"
-//                                     name="email"
-//                                     onChange={this.onChange}
-//                                     value={this.state.email}
-//                                 />
-//                             </div>
-//
-//                             <div className="form-group">
-//                                 <label htmlFor="phone">Phone</label>
-//                                 <imput
-//                                     type="text"
-//                                     className="form-control"
-//                                     name="phone"
-//                                     minLength="10"
-//                                     required
-//                                     onChange={this.onChange}
-//                                     value={this.state.phone}
-//                                 />
-//                             </div>
-//
-//                             <div className="form-group">
-//                                 <label htmlFor="balance">Balance</label>
-//                                 <imput
-//                                     type="text"
-//                                     className="form-control"
-//                                     name="balance"
-//                                     onChange={this.onChange}
-//                                     value={this.state.balance}
-//                                 />
-//                             </div>
-//
-//                             <input
-//                                 type="submit"
-//                                 value="Submit"
-//                                 className="btn btn-primary btn-block"
-//                             />
-//                         </form>
-//                     </div>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
 }
+
+AddClient.propTypes = {
+    firestore: PropTypes.object.isRequired
+};
 
 export default firestoreConnect()(AddClient);
